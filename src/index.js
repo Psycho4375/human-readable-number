60a119ae70ac8071699f3ecd5cbdeb161eeb7a2b
Psycho4375 <119705159+Psycho4375@users.noreply.github.com>
2023-01-21 14:30:26 +0300

    fix: fourty =>  forty

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 module.exports = function toReadable (number) {
     let toTwelve = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
-    let toTens = ["twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+    let toTens = ["twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
     let toHundreds = ["one hundred", "two hundred", "three hundred","four hundred", "five hundred", "six hundred", "seven hundred", "eight hundred", "nine hundred"];
     
     let nTotwelve;
